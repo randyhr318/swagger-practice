e224659d96ddfdb6227cb0d070b6fb8f032cd45c
Randy Roberson <Randyhr318@gmail.com>
2021-03-03 13:14:46 -0600

    Initial commit

---
+++ src/test/java/com/tts/swagger/practice/SwaggerPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.swagger.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
